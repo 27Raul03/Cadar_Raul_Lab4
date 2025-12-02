026e7ff9d690328e845b4f3a02866b2eac46c1c2
IDEAPAD-FLEX-5\cadar <cadarraulvladut27@gmail.com>
2025-12-02 13:51:22 +0200

    Curs 6 / Exercitiul 1

---
+++ Controllers/PredictionApiController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Cadar_Raul_Lab4.Data;
+using Cadar_Raul_Lab4.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Cadar_Raul_Lab4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PredictionApiController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PredictionApiController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<PredictionHistory>>> GetAll()
+        {
+            var list = await _context.PredictionHistories
+                                      .OrderByDescending(p => p.CreatedAt)
+                                      .ToListAsync();
+
+            return Ok(list);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var prediction = await _context.PredictionHistories.FindAsync(id);
+
+            if (prediction == null)
+                return NotFound(new { message = "Prediction not found." });
+
+            _context.PredictionHistories.Remove(prediction);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Prediction deleted successfully." });
+        }
+
+    }
+}
