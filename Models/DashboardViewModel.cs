4397b7dd38068d241154aea21048631e81f907dd
IDEAPAD-FLEX-5\cadar <cadarraulvladut27@gmail.com>
2025-12-05 17:39:52 +0200

    Lab 9 DONE

---
+++ Models/DashboardViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Cadar_Raul_Lab4.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalPredictions { get; set; }
+        public List<PaymentTypeStat> PaymentTypeStats { get; set; } = new();
+        public List<PriceBucketStat> PriceBuckets { get; set; }
+    }
+
+    public class PaymentTypeStat
+    {
+        public string PaymentType { get; set; } = string.Empty;
+        public double AveragePrice { get; set; }
+        public int Count { get; set; }
+    }
+
+    public class PriceBucketStat
+    {
+        public string Label { get; set; } = string.Empty; // ex. "0-10", "10-20"
+        public int Count { get; set; }
+    }
+}
